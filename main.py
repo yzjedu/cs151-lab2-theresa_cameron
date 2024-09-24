8724354ea004b73c8453ee80b4e80bfd92953b5a
theresadejacimo <tadejacimo@loyola.edu>
2024-09-24 12:35:56 -0400

    Finished and Tested Code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Delete these three lines before submitting
 
 #enter variables for birth, death and immigration rates
+seconds_per_year = 31536000
 birth_rate = int(input("please enter birth rate: "))
 death_rate = int(input("please enter death rate: "))
 immigration = int(input("please enter immigrations rate: "))
@@ -13,17 +14,17 @@ num_years = int(input("please enter number of projected years in future: "))
 print("")
 
 #calculations for populations change and future populations
-pop_change =
-future_pop =
+pop_change = (seconds_per_year/birth_rate + seconds_per_year/immigration - seconds_per_year/death_rate) * num_years
+future_pop = pop_change + current_pop
 
 #output for populations change and future population
 print("\nthe future population is ",future_pop, "and the change in population is", pop_change)
 
 
 #population increase or decrease, or no change in population
-if current_pop >= future_pop:
+if current_pop < future_pop:
     print("population increased")
-elif current_pop <= future_pop:
+elif current_pop > future_pop:
     print("populations decreased")
 else:
     print("population did not change")
